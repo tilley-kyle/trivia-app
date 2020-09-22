b0ccba17083913b9d7df42ebc7c01681285bf96e
Kyle Tilley <katilley10@gmail.com>
2020-09-22 16:07:46 -0600

    done for the day, need to center the trivia button and then add the page that will actually show questions for a user

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,27 +3,32 @@ import React from 'react';
 const Home = ({ categories }) => {
   console.log(categories)
   return (
-    <div className="option-boxes">
-      <form>
-        <input list="categories" />
-        <datalist id="categories">
-          {/* {categories.forEach((category) => {
-            return <option value="category" />
-          })} */}
-        </datalist>
-      </form>
-      <form>
-        <input type="number" placeholder="Less than 50" min="1" max="50" />
-      </form>
-      <form>
-        <input list="question-count" />
-        <datalist id="question-count">
-          <option value="any" />
-          <option value="Easy" />
-          <option value="Medium" />
-          <option value="Difficult" />
-        </datalist>
-      </form>
+    <div>
+      <div className="option-boxes">
+        <form>
+          <input list="categories" placeholder="Select Category" />
+          <datalist id="categories">
+            {categories.trivia_categories.map((category) => {
+              return <option value={category.name} key={category.id} />
+            })}
+          </datalist>
+        </form>
+        <form>
+          <input type="number" placeholder="Number of Questions" min="1" max="50" />
+        </form>
+        <form>
+          <input list="difficulty" placeholder="Difficulty" />
+          <datalist id="difficulty">
+            <option value="any" />
+            <option value="Easy" />
+            <option value="Medium" />
+            <option value="Difficult" />
+          </datalist>
+        </form>
+      </div>
+      <div className="btn-cont">
+      <button className="trivia-bttn">TRIVIA!!</button>
+      </div>
     </div>
   )
 }
